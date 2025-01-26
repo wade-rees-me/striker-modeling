384b4897218dff90db86c9b244633488d6d5a861
wade-rees-me <134008725+wade-rees-me@users.noreply.github.com>
2025-01-25 18:03:16 -0700

    Version update

--- src/constants/constants.hpp
+++ src/constants/constants.hpp
@@ -6,7 +6,7 @@
 
 // General constants
 const static std::string STRIKER_WHO_AM_I = "striker-c-plus-plus";
-const static std::string STRIKER_VERSION = "v2.02.00";	 // Epoch.Major.Minor
+const static std::string STRIKER_VERSION = "v2.03.00";	 // Epoch.Major.Minor
 const static char* TIME_LAYOUT = "%Y-%m-%d %H:%M:%S %z";
 
 // Define the maximum sizesstring fields
